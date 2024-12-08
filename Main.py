54c217ee06bc44e5f3ccf05247c423ce545aab6b
Jakub Hac <jakub@hacah.dev>
2024-12-08 15:20:00 +0100

    remove rant from main

--- Main.py
+++ Main.py
@@ -8,14 +8,6 @@ Init.setup_screen()
 Init.setup_clock()
 Init.init_state()
 
-b = [(1, 2), (3, 4)]
-a = [(1, 2)]
-
-is_python_a_viable_language_for_anything = a in b
-do_i_have_to_do_stupid_things_that_should_just_be_properly_implemented = PythonUtils.is_a_sublist_of_b(a, b)
-print(is_python_a_viable_language_for_anything)
-print(do_i_have_to_do_stupid_things_that_should_just_be_properly_implemented)
-
 while not State.close_requested:
     input_events = pygame.event.get()
     for event in input_events:
